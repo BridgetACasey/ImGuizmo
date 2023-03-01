7c505a30b17c431de3116e8404e9625afaa9455d
BridgetACasey <bridgetannabelcasey@yahoo.com>
2023-03-01 22:23:52 +0000

    Added premake5 file and updated .gitignore

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "on"
+
+	targetdir ("build/" .. outputdir .. "/%{prj.name}")
+	objdir ("build-int/" .. outputdir .. "/%{prj.name}")
+
+   includedirs
+   {
+      "example"
+   }
+
+	files
+	{
+		"GraphEditor.h",
+		"GraphEditor.cpp",
+      "ImCurveEdit.h",
+      "ImCurveEdit.cpp",
+      "ImGradient.h",
+      "ImGradient.cpp",
+      "ImGuizmo.h",
+      "ImGuizmo.cpp",
+      "ImSequencer.h",
+      "ImSequencer.cpp",
+      "ImZoomSlider.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
